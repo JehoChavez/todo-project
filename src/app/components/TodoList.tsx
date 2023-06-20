cf6dcbf3686b5a64ae22abcc0017f224ecb37f9b
Jeho Chavez <chavezjehonatan@gmail.com>
2023-06-19 21:15:31 -0600

    Removed filters

--- src/app/components/TodoList.tsx
+++ src/app/components/TodoList.tsx
@@ -2,55 +2,24 @@
 
 import fetchTodos from "@/lib/fetchTodos";
 
-import { useEffect, useState } from "react";
-
 import TodoItem from "./TodoItem";
-import Filters from "./Filters";
 
-export default function TodoList() {
+export default async function TodoList() {
+  const todos = await fetchTodos();
+
   let content;
 
-  const [initialTodos, setInitalTodos] = useState<Todo[] | undefined>(
-    undefined
-  );
-  const [displayedTodos, setDisplayedTodos] = useState<Todo[] | undefined>(
-    undefined
-  );
-
-  useEffect(() => {
-    const fetchMyTodos = async () => {
-      const todos = await fetchTodos();
-      setInitalTodos(todos);
-      setDisplayedTodos(todos);
-    };
-
-    fetchMyTodos();
-  }, []);
-
-  const filterTodos = (filteredTodos: Todo[]) => {
-    setDisplayedTodos(filteredTodos);
-  };
-
-  if (!initialTodos) {
-    content = <p>Loading...</p>;
-  } else if (initialTodos.length === 0 || displayedTodos === undefined) {
+  if (todos === undefined) {
     content = <p>No todos available</p>;
   } else {
-    const sortedTodos = displayedTodos.reverse();
-
     content = (
       <ul>
-        {sortedTodos.map((todo) => (
-          <TodoItem todo={todo}  key={todo.id}/>
+        {todos.map((todo) => (
+          <TodoItem todo={todo} key={todo.id} />
         ))}
       </ul>
     );
   }
 
-  return (
-    <>
-      <Filters todos={initialTodos} filterFunction={filterTodos} />
-      {content}
-    </>
-  );
+  return <>{content}</>;
 }
